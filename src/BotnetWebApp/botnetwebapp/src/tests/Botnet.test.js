f6a3c5be421a6422e1c184038eb6a9d658701acc
Bryssinck,Robbe R <robbebryssinck@gmail.com>
2020-12-03 13:00:25 +0100

    Experimental tag on exploit functionality

--- src/BotnetWebApp/botnetwebapp/src/tests/Botnet.test.js
+++ src/BotnetWebApp/botnetwebapp/src/tests/Botnet.test.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
@@ -7,11 +8,13 @@ import Botnet from "../components/Botnet";
 jest.mock("axios");
 
 describe("Botnet", () => {
-  test("display message if botnet does not exist", () => {
+  test("display message if botnet does not exist", async () => {
     axios.get.mockImplementation(() => Promise.reject(new Error()));
     render(<Botnet />);
 
-    const message = await screen.findByText(/The requested botnet is not available or does not exist./);
+    const message = await screen.findByText(
+      /The requested botnet is not available or does not exist./
+    );
 
     expect(message).toBeInTheDocument();
   });
